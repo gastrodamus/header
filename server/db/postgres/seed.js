73a32809c78dee3d27c175b4979f164812a8d120
esme <esme@github.com>
2019-07-25 16:36:46 -0700

    change schema to reflect many to many relationship between restaurants and categories

--- server/db/postgres/seed.js
+++ server/db/postgres/seed.js
@@ -1,7 +1,7 @@
 const faker = require('faker');
 const fs = require('fs');
-const { Pool, Client } = require('pg');
 const { mean } = require('mathjs');
+const path = require('path');
 
 const makeRestaurantName = () => {
   const foodTypes = ['Pizza', 'Steak', 'Brunch', 'Seafood', 'Italian', 'Chinese', 'Japanese', 'Korean', 'Seafood', 'Fish', 'Pho', 'Noodle', 'Ramen'];
@@ -11,93 +11,106 @@ const makeRestaurantName = () => {
   return `${adjective} ${foodTypes[Math.floor(Math.random() * foodTypes.length)]} ${foodPlaces[Math.floor(Math.random() * foodPlaces.length)]}`;
 };
 
-const createReviews = (i, writeStreamReview) => {
+const createData = (i, writeStream, writeStreamReview, writeStreamJoin) => {
   const month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-  const year = ['2015', '2016', '2017', '2018', '2019'];
   const stars = [];
 
-  for (let j = 0; j < year.length; j += 1) { // Generates a random review from each month for the past 5 years
-    for (let k = 0; k < month.length; k += 1) {
-      const id = i + 1;
+  const restaurantId = i + 1;
+  let reviewData = '';
+
+  for (let m = 0; m < 6; m += 1) { // Generates reviews for past 6 months
+    const reviewsPerMth = faker.random.number({ min: 1, max: 20 });
+    for (let j = 0; j < reviewsPerMth; j += 1) { // Generates random reviews for each month
+      const date = month[m].concat('-', faker.random.number({ min: 1, max: 28 }).toString().concat('-', '2019'));
       const star = faker.random.number({ min: 1, max: 5 });
       stars.push(star);
-      const date = month[k].concat('-', faker.random.number({ min: 1, max: 28 }).toString().concat('-', year[j]));
-      let data = [id, star, date].join(',');
-      data += '\n';
-      writeStreamReview.write(data, 'utf-8');
+      reviewData += [restaurantId, star, date].join(',');
+      reviewData += '\n';
     }
   }
+  writeStreamReview.write(reviewData, 'utf-8');
+
+  let restaurantData = [
+    restaurantId,
+    makeRestaurantName(),
+    Math.round(mean(stars) * 10) / 10,
+    faker.random.number({ min: 10, max: 100 }),
+  ].join(',');
+  restaurantData += '\n';
+  writeStream.write(restaurantData, 'utf-8');
+
+  const categoryIdsArr = [null, null, null];
+  const categoryIdsObj = {};
+
+  const generateUniqueId = () => {
+    const result = faker.random.number({ min: 1, max: 50 });
+    if (categoryIdsObj[result]) {
+      generateUniqueId();
+    } else {
+      categoryIdsObj[result] = true;
+    }
+    return result;
+  };
+
+  categoryIdsArr[0] = generateUniqueId();
+  categoryIdsArr[1] = generateUniqueId();
+  categoryIdsArr[2] = generateUniqueId();
+
+  let joinData = '';
+
+  for (let k = 0; k < 3; k += 1) { // Generates 3 categories for one restaurant
+    joinData += [
+      restaurantId,
+      categoryIdsArr[k],
+    ].join(',');
+    joinData += '\n';
+  }
+  return writeStreamJoin.write(joinData, 'utf-8');
 };
 
-writeOneMillionTimes();
+function createCategories(writeStreamCategory) {
+  let categoryData = '';
+  for (let l = 0; l < 50; l += 1) {
+    const category = [];
+    category.push(l + 1);
+    category.push(faker.lorem.word());
+    categoryData += category.join(',');
+    categoryData += '\n';
+  }
+  writeStreamCategory.write(categoryData, 'utf-8');
+}
 
-function writeOneMillionTimes() {
-  let i = 10000000;
-  const encoding = 'utf-8';
-  const writeStream = fs.createWriteStream('./server/csv/business.csv');
-  const writeStreamReview = fs.createWriteStream('./server/csv/review.csv');
+function writeData() {
+  console.time('calculationTime');
+  let i = 1;
+  const writeStream = fs.createWriteStream(path.resolve(__dirname, '../csv/business.csv'));
+  const writeStreamJoin = fs.createWriteStream(path.resolve(__dirname, '../csv/join.csv'));
+  const writeStreamReview = fs.createWriteStream(path.resolve(__dirname, '../csv/review.csv'));
+  const writeStreamCategory = fs.createWriteStream(path.resolve(__dirname, '../csv/category.csv'));
 
-  write();
+  createCategories(writeStreamCategory);
+
   function write() {
     let ok = true;
-    let okReview = true;
     do {
-      i--;
-      let dataItem = [
-        makeRestaurantName(),
-        // average,
-        faker.random.number({ min: 1, max: 4 }),
-        faker.lorem.words(),
-      ].join(',');
-      dataItem += '\n';
+      i -= 1;
       if (i === 0) {
         // Last time!
-        writeStream.write(dataItem, encoding);
-        createReviews(i, writeStreamReview);
+        createData(i, writeStream, writeStreamReview, writeStreamJoin);
+        console.timeEnd('calculationTime');
       } else {
         // See if we should continue, or wait.
         // Don't pass the callback, because we're not done yet.
-        ok = writeStream.write(dataItem, encoding);
-        okReview = createReviews(i, writeStreamReview);
+        ok = createData(i, writeStream, writeStreamReview, writeStreamJoin);
       }
-    } while (i > 0 && ok && okReview);
+    } while (i > 0 && ok);
     if (i > 0) {
       // Had to stop early!
       // Write some more once it drains.
       writeStream.once('drain', write);
     }
   }
+  write();
 }
 
-// const pool = new Pool({
-//   user: 'root',
-//   host: 'localhost',
-//   database: 'business',
-//   password: 'password',
-// });
-
-// (async () => {
-//   const client = await pool.connect();
-
-//   try {
-//     await client.query('BEGIN');
-//     for (let i = 0; i < 100000; i += 1) {
-//       const query = {
-//         text: 'INSERT INTO business_schema.items(name, avg_stars, price, categories) VALUES($1, $2, $3, $4)',
-//         values: [
-//           makeRestaurantName(),
-//           faker.random.number({ min: 1, max: 5 }),
-//           faker.random.number({ min: 1, max: 4 }),
-//           faker.lorem.words(),
-//         ],
-//       };
-//       await client.query(query);
-//     }
-//     await client.query('COMMIT');
-//   } catch (e) {
-//     await client.query('ROLLBACK');
-//     throw e;
-//   } finally {
-//     client.release();
-//   }
-// })().catch(e => console.log(e.stack));
+writeData();
