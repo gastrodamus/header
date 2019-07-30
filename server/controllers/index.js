13cfb018d565c222ae50d177b4f43ff9a74c69a4
esme <23175119+esme@users.noreply.github.com>
2019-07-29 20:17:32 -0700

    add controllers for querying

---
+++ server/controllers/index.js
@@ -0,0 +1,91 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'root',
+  host: 'localhost',
+  database: 'header',
+  password: 'password',
+});
+
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+const getHeader = async (req, res) => {
+  const header = await pool.query(`SELECT * FROM restaurant WHERE restaurant_id = ${req.params.id}`);
+  res.send(header.rows);
+};
+
+const getCategory = async (req, res) => {
+  const categoryIds = await pool.query(`SELECT category_id FROM restaurant_category WHERE restaurant_id = ${req.params.id}`);
+  const arr1 = categoryIds.rows.map(el => el.category_id);
+  const category = await pool.query(`SELECT * FROM category WHERE category_id in (${arr1[0]}, ${arr1[1]}, ${arr1[2]})`);
+  res.send(category.rows);
+};
+
+const getReview = async (req, res) => {
+
+};
+
+const postCategory = async (req, res) => {
+  // req.body example: {"catId": 2}
+  // update with new category id
+  await pool.query(
+    `INSERT INTO restaurant_category (restaurant_id, category_id) 
+    VALUES (${req.params.id}, ${req.body.catId})`,
+  );
+  res.send('successfully added category');
+};
+
+const postHeader = async (req, res) => {
+  // req.body example: {"resName": "Seafood", "resPrice": 35}
+  await pool.query(
+    `INSERT INTO restaurant (restaurant_name, price) 
+    VALUES (${req.body.resName}, ${req.body.resPrice})`,
+  );
+  res.send('successfully added restaurant');
+};
+
+const patchCategory = async (req, res) => {
+  // req.body example: {"oldCatId": 2, "newCat": "Singaporean"}
+  // retrieve new category id
+  const newCat = await pool.query(`SELECT category_id FROM category WHERE category_name = '${req.body.newCat}'`);
+  // update with new category id
+  await pool.query(
+    `UPDATE restaurant_category 
+    SET category_id = ${newCat.rows[0].category_id}
+    WHERE restaurant_id = ${req.params.id}
+    AND category_id = ${req.body.oldCatId}`,
+  );
+  res.send('successfully updated category');
+};
+
+const deleteHeader = async (req, res) => {
+  await pool.query(
+    `DELETE FROM restaurant 
+    WHERE restaurant_id = ${req.body.resId})`,
+  );
+  res.send('successfully added restaurant');
+};
+
+const deleteCategory = async (req, res) => {
+  // req.body example: {"catId": 2, "resId": 1}
+  await pool.query(
+    `DELETE FROM restaurant_category 
+    WHERE restaurant_id = '${req.params.id}' 
+    AND category_id = '${req.body.catId}'`,
+  );
+  res.send('successfully deleted category');
+};
+
+module.exports = {
+  getHeader,
+  getCategory,
+  getReview,
+  postCategory,
+  postHeader,
+  patchCategory,
+  deleteHeader,
+  deleteCategory,
+};
