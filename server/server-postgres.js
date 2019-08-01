1838c820e99d32d6a651845fa374ed67553f1fb1
esme <esme@github.com>
2019-07-31 20:06:04 -0700

    rename routes

--- server/server-postgres.js
+++ server/server-postgres.js
@@ -1,3 +1,4 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const controllers = require('./controllers/postgres');
 
@@ -5,20 +6,21 @@ const router = express.Router();
 const app = express();
 const port = 3003;
 
-app.use('/api', router);
+app.use('/api/header', router);
 app.use(express.json());
 
-router.get('/:id/header', controllers.getRestaurant);
+router.get('/', controllers.getRestaurantList);
+router.get('/:id/restaurant', controllers.getRestaurant);
 router.get('/:id/category', controllers.getCategories);
-router.get('/:id/review', controllers.getReviews);
+router.get('/:id/reviews', controllers.getReviews);
 
-router.post('/:id/header', controllers.postRestaurant);
+router.post('/:id/restaurant', controllers.postRestaurant);
 router.post('/:id/category/:catId', controllers.postCategory);
 
-router.patch('/:id/header', controllers.patchRestaurant);
+router.patch('/:id/restaurant', controllers.patchRestaurant);
 router.patch('/:id/category', controllers.patchCategory);
 
-router.delete('/:id/header', controllers.deleteRestaurant);
+router.delete('/:id/restaurant', controllers.deleteRestaurant);
 router.delete('/:id/category/:catId', controllers.deleteCategory);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
