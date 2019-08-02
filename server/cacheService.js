a406ae039936df683b34216cded574ea1cffcacf
esme <23175119+esme@users.noreply.github.com>
2019-08-02 15:25:13 -0700

    add redis cache

---
+++ server/cacheService.js
@@ -0,0 +1,21 @@
+// cache service
+const redis = require('redis');
+
+const client = redis.createClient();
+
+function cache(req, res, next) {
+  const key = req.method + req.originalUrl;
+  client.get(key, (err, data) => {
+    if (err) throw err;
+    if (data != null) {
+      res.send(JSON.parse(data));
+    } else {
+      next();
+    }
+  });
+}
+
+module.exports = {
+  client,
+  cache,
+};
